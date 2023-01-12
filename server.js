28f144e886cca5c9c01b0b9c7eb3a66b6113b07c
RealGoldenGeneral <ncheng25@my.bcit.ca>
2023-01-12 10:58:09 -0800

    added chatbot route

--- server.js
+++ server.js
@@ -2,3 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
+
+app.post('/chatbot', (req, res) => {
+    const message = req.body.message;
+    const number = message.match(/\+d/);
+    if (number) {
+        fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data =>{
+            res.json({
+                text: data
+            });
+        }).catch(error => {
+            res.json({
+                text: "Sorry, I couldn't find information about that number"
+            })
+        })
+    } else {
+        res.json({
+            text: "I'm sorry, I didn't understand your question. Please provide a number for me to give information about."
+        })
+    }
+});
